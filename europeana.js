91daba6a8c46629f142dbc659c5399d53eea93f1
Franklin van de Meent <fr@nkl.in>
2015-03-03 09:17:23 +0100

    Rewrite all code
    * Interface: module is now function for setup, returns function for API calls
    * Reduced code complexity

--- europeana.js
+++ europeana.js
@@ -10,11 +10,18 @@ License:      Public Domain / Unlicense (see UNLICENSE file)
 
 var http = require ('http');
 var querystring = require ('querystring');
-module.exports = {};
+
+var settings = {
+  apikey: null,
+  timeout: 5000
+};
 
 // config
-module.exports.timeout = 5000;
-module.exports.apikey = null;
+module.exports = function setup (apikey, timeout) {
+  settings.apikey = apikey || null;
+  settings.timeout = timeout || settings.timeout;
+  return talk;
+};
 
 
 // errors
@@ -29,43 +36,14 @@ var errors = {
 };
 
 
-// get one item
-// http://www.europeana.eu/portal/api-record-json.html
-module.exports.record = function (id, profile, callback) {
-  if (typeof profile === 'function') {
-    var callback = profile;
-    var profile = 'full';
-  }
-  talk ('record/'+ id, {profile: profile}, callback);
-};
-
-// search items
-// http://www.europeana.eu/portal/api-search-json.html
-module.exports.search = function (query, vars, callback) {
-  if (typeof vars === 'function') {
-    var callback = vars;
-    var vars = {};
-  }
-  vars.query = query;
-  talk ('search', vars, callback);
-};
-
-// livesearch suggestions
-// http://www.europeana.eu/portal/api-suggestions-json.html
-module.exports.suggestions = function (query, rows, callback) {
-  if (typeof rows === 'function') {
-    var callback = rows;
-    var rows = 10;
-  }
-  talk ('suggestions', {query: query, rows: rows}, callback);
-};
-
-
 // communicate with the API
 function talk (path, fields, callback) {
+  var data = [];
+  var size = 0;
+
   if (typeof fields === 'function') {
-    var callback = fields;
-    var fields = {};
+    callback = fields;
+    fields = {};
   }
 
   // prevent multiple callbacks
@@ -77,6 +55,73 @@ function talk (path, fields, callback) {
     }
   }
 
+
+  // Handlers
+  function onSocket (socket) {
+    socket.setTimeout (parseInt (settings.timeout));
+    socket.on ('timeout', function () {
+      doCallback (new Error ('request timeout'));
+      request.abort ();
+    });
+  }
+
+  function onError (e) {
+    var err = new Error ('request failed');
+    err.error = error;
+    doCallback (err);
+  }
+
+  function onResponse (response) {
+    response.on ('close', onClose);
+    response.on ('data', onData);
+    response.on ('end', onEnd);
+  }
+
+  function onClose () {
+    doCallback (new Error ('request dropped'));
+  }
+
+  function onData (ch) {
+    data.push (ch);
+    size += ch.length;
+  }
+
+  function onEnd () {
+    var er = null;
+    data = Buffer.concat (data, size) .toString () .trim ();
+
+    if (response.statusCode !== 200) {
+      er = new Error ('API error');
+      er.code = response.statusCode;
+      er.error = errors [response.statusCode];
+
+      if (data.match (/<h1>HTTP Status /)) {
+        er.error = data.replace (/.*<b>description<\/b> <u>(.+)<\/u><\/p>.*/, '$1');
+      }
+    } else {
+      try {
+        data = JSON.parse (data);
+        if (!data.success && data.error) {
+          er = new Error ('API error');
+          err.error = data.error;
+          err.data = data;
+          data = null;
+        }
+      } catch (reason) {
+        // can't read this
+        er = new Error ('invalid response');
+        er.error = reason;
+        er.data = data;
+        data = null;
+      }
+    }
+
+    doCallback (er, data);
+  }
+
+
+  // Request
+
   // check API key
   if (!module.exports.apikey) {
     doCallback (new Error ('apikey missing'));
@@ -84,8 +129,7 @@ function talk (path, fields, callback) {
   }
 
   // build request
-  fields.wskey = module.exports.apikey;
-
+  fields.wskey = settings.apikey;
   var query = '?'+ querystring.stringify (fields);
 
   var options = {
@@ -99,74 +143,8 @@ function talk (path, fields, callback) {
   };
 
   var request = http.request (options);
-
-  // request failed
-  request.on ('error', function (error) {
-    var err = new Error ('request failed');
-    err.error = error;
-    doCallback (err);
-  });
-
-  // request timeout
-  request.on ('socket', function (socket) {
-    socket.setTimeout (module.exports.timeout);
-    socket.on ('timeout', function () {
-      request.abort ();
-      doCallback (new Error ('request timeout'));
-    });
-  });
-
-  // response
-  request.on ('response', function (response) {
-    var data = [];
-    var size = 0;
-
-    // too early disconnected
-    response.on ('close', function () {
-      doCallback (new Error ('request dropped'));
-    });
-
-    // receiving data
-    response.on ('data', function (ch) {
-      data.push (ch);
-      size += ch.length;
-    });
-
-    // process response data
-    response.on ('end', function () {
-      var er = null;
-      data = Buffer.concat (data, size) .toString () .trim ();
-
-      if (response.statusCode !== 200) {
-        er = new Error ('API error');
-        er.code = response.statusCode;
-        er.error = errors [response.statusCode];
-
-        if (data.match (/<h1>HTTP Status /)) {
-          er.error = data.replace (/.*<b>description<\/b> <u>(.+)<\/u><\/p>.*/, '$1');
-        }
-      } else {
-        try {
-          data = JSON.parse (data);
-          if (!data.success && data.error) {
-            er = new Error ('API error');
-            err.error = data.error;
-            err.data = data;
-            data = null;
-          }
-        } catch (reason) {
-          // can't read this
-          er = new Error ('invalid response');
-          er.error = reason;
-          er.data = data;
-          data = null;
-        }
-      }
-
-      doCallback (er, data);
-    });
-  });
-
-  // do it
+  request.on ('error', onError);
+  request.on ('socket', onSocket);
+  request.on ('response', onResponse);
   request.end ();
 }
